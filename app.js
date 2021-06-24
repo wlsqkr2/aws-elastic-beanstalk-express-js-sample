5608ce94c006160477a339e47e892b966ef19c75
박진 <jp2jjj@gmail.com>
2021-06-24 13:03:38 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Wooooooooorld!'));
+app.get('/', (req, res) => res.send('Hellooooooooooooooooooooooo Wooooooooorld!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
